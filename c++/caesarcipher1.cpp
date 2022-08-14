d9ba2c22aba63114ef725e317132d8d5401c763d
Sue Zaw <techwithsue271007@gmail.com>
2022-08-14 16:54:32 +0630

    C++ caesarcipher sustitution cipher exercise

---
+++ c++/caesarcipher1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+int main() {
+
+	char plaintext[26] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
+	char ciphertext[26] ={ 'z','w','k','u','g','i','e','o','f','m','c','4','j','0','h','@','1','s','r','7','d','x','b','v','3','a' };
+	char ch = '\0';
+	char index = '\0';
+	std::cout<<"Add your favourite alphabet from English alphabets\t";
+	std::cin>>ch;
+	
+	for (int i = 0; i < 26; i++) {
+		if (ch == plaintext[i]) {
+			index = i;
+			break;
+		}
+	}
+
+	std::cout << "This is Encrypted Text..\t"<< ciphertext[index]<<"\n\n";
+	}
