54af58dbceba7fa6328a854faee99c09efa77010
Sue Zaw <techwithsue271007@gmail.com>
2022-09-16 07:53:31 +0630

    strlwr and strupr

--- stringManipulation/Source.cpp
+++ stringManipulation/Source.cpp
@@ -3,11 +3,20 @@
 
 int main() {
 	char a[] = { 'p','r','o','g','r','a','m','\0' };
-char b[]= "programs";
-char c[] = "programmer";
-printf("a=%d b=%d", strlen2(a), strlen2(b));
-char* j;
-j = strcpy2(a, c);
-printf("a is now =%s", j);
-
+	char b[] = "programs";
+	char c[] = "programmer";
+	char d[] = "PROGRAMMERRR";
+	printf("a=%d b=%d", strlen2(a), strlen2(b));
+	char* j;
+	j = strcpy2(a, c);
+	printf("a is now =%s", j);
+	/*char* g;
+	g = strcat2(c, a);
+	printf("%c" = g);*/
+	char *l;
+	l = strupr(b);
+	printf("\n%s", l);
+	char* k;
+	k = strlwr(d);
+	printf("\n%s", k);
 }
--- stringManipulation/strManiHeader.h
+++ stringManipulation/strManiHeader.h
@@ -1,4 +1,7 @@
 
-int strlen2(char* arr);
-char* strcpy2(char* destination, char* const source);
-//char* strcat2(char* destination, char* source);
+int strlen2(const char* arr);
+char* strcpy2(char* destination, const char*  source);
+//int strcmp2(const char* str1, const char* str2);
+//char* strcat2(char* destination, const char* source);
+char* strlwr(char* s);
+char* strupr(char* s);
